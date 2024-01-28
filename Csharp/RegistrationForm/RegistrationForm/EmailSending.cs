19276dda1dcda847354db8d836606b12481bc5d9
TANMES <tanmesd@mindfiresolutions.com>
2024-01-28 23:40:43 +0530

    Added Email sending Program

---
+++ Csharp/RegistrationForm/RegistrationForm/EmailSending.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegistrationForm
+{
+     class EmailSending
+    {
+        static void Main(string[] args)
+        {
+            EmailSending emailSending = new EmailSending();
+        }
+    }
+}
