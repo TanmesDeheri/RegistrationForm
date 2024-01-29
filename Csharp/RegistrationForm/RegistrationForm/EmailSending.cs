8f8c6dd31d25cd1a0daf959e0dd757510dc66a7b
TANMES <tanmesd@mindfiresolutions.com>
2024-01-29 10:09:02 +0530

    Added Code for Testing Email

--- Csharp/RegistrationForm/RegistrationForm/EmailSending.cs
+++ Csharp/RegistrationForm/RegistrationForm/EmailSending.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MailKit.Net.Smtp;
+using MimeKit;
 
 namespace RegistrationForm
 {
@@ -11,6 +13,34 @@ namespace RegistrationForm
         static void Main(string[] args)
         {
             EmailSending emailSending = new EmailSending();
+            emailSending.TestEmail();
+        }
+        void TestEmail()
+        {
+            try
+            {
+            var client = new SmtpClient();
+            client.Connect("sandbox.smtp.mailtrap.io", 25, false);
+            client.Authenticate("779b290f109a4b", "737dc2ebdd1809");
+            var bodyBuilder = new BodyBuilder()
+            {
+                TextBody = "Hello,its my first mail testing from Tanmes"
+            };
+            var sendEmail = new MimeMessage()
+            {
+                From = { new MailboxAddress("from", "from@example.com") },
+                To = { new MailboxAddress("To", "To@example.com") },
+                Subject = "Testing Email",
+                Body=bodyBuilder.ToMessageBody()
+            };
+            client.Send(sendEmail);
+            client.Disconnect(true);
+                Console.WriteLine("Email Sent");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
         }
     }
 }
