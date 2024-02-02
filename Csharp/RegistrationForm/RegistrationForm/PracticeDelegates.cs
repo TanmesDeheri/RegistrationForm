8d2513abcc012bed2822e0a123751bcaa4e2bc09
TANMES <tanmesd@mindfiresolutions.com>
2024-02-02 18:25:44 +0530

    Added layer DB Assignment and Practice EF Files

---
+++ Csharp/RegistrationForm/RegistrationForm/PracticeDelegates.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace RegistrationForm
+{
+    class PracticeDelegates
+    {
+        public delegate void MethodForDelegate();
+        public static void Main(string[] args)
+        {
+            MethodForDelegate del = new MethodForDelegate(Test);
+            PracticeClass.MethodCallFromPracticeDelegates(del);
+        }
+
+        private static void Test()
+        {
+            Console.WriteLine("Hi , This is a Test Method");
+        }
+    }
+    class PracticeClass
+    {
+        public static void MethodCallFromPracticeDelegates(MethodForDelegate del)
+        {
+            del.invoke();
+        } 
+    }
+}
