44ca0458977e6a9b2e8c145dcaa1534076d2bd05
TANMES <tanmesd@mindfiresolutions.com>
2024-02-05 23:49:24 +0530

    Added Asp.net Files

---
+++ PracticeWebApplication/PracticeWebApplication/FirstPage.aspx.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace PracticeWebApplication
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            userInput.Text = "";
+            genderId.Text = "";
+            ShowDate.Text = "";
+            ShowCourses.Text = "None";
+        }
+        protected void SubmitButtonClick(object sender, EventArgs e)
+        {
+            userInput.Text = UserName.Text;// Value i sfetched from the input and Stored in the label
+            if (RadioButton1.Checked)//checking Gender
+            {
+                genderId.Text = "Your gender is " + RadioButton1.Text;
+            }
+            else genderId.Text = "Your gender is " + RadioButton2.Text;
+            ShowDate.Text = "You Selected: " + Calendar1.SelectedDate.ToString("D");
+            var message = "";
+            if (CheckBox1.Checked)
+            {
+                message = CheckBox1.Text + " ";
+            }
+            if (CheckBox2.Checked)
+            {
+                message += CheckBox2.Text + " ";
+            }
+            if (CheckBox3.Checked)
+            {
+                message += CheckBox3.Text;
+            }
+            ShowCourses.Text = message;
+        }
+    }
+}
